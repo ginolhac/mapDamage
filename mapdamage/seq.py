a884a43c5383361eb073a7ae86f41715a0c4b3dc
Mikkel Schubert <MikkelSch@gmail.com>
2012-11-19 14:50:14 +0100

    Improve comparison of FASTA/BAM sequences dicts.

--- mapdamage/seq.py
+++ mapdamage/seq.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import sys
 import string
 
 # from Martin Kircher, to complement DNA
@@ -39,28 +39,49 @@ def recordLg(read, coordinate, tab):
   return tab
 
 
-def fastaIndex(f):
+def read_fasta_index(filename):
   """ from a fasta index file, fai, return dictionary of references:lengths """
+  def print_err(msg, filename, line):
+    sys.stderr.write("Error: %s\n" % msg)
+    sys.stderr.write("       Filename: %s\n" % filename)
+    sys.stderr.write("       Line:     %s\n" % repr(line))
   
   fai = {}
-  with open(f, 'r') as fh:
-    for line in fh:
-      ref = line.strip().split('\t')
+  with open(filename, 'r') as handle:
+    for line in handle:
+      ref = line.split("\t")
+      if len(ref) != 5:
+        print_err("Line in fasta index contains wrong number of fields, found %i, expected 5:" % len(ref), filename, line)
+        return None
+
       try:
-        fai[ref[0]] = ref[1]
-      except:
-        sys.stderr.write("Error: fai file is not correct %s\n" % f)
+        fai[ref[0]] = int(ref[1])
+      except ValueError:
+        print_err("Column 2 in FASTA index did not contain a number, found '%s':" % ref[1], filename, line)
+        return None
 
   return fai
 
 
-def checkRef(failengths, reflengths):
-  """ check if all references in BAM/SAM are present in the fasta reference with same length """
-  for ref in reflengths:
-    lg = int(failengths.get(ref, 0))
-    if lg != reflengths[ref]:
-      print("\nreference %s is not correct in the fasta file" % ref)
-      return None
+def describe_sequence_dicts(fasta_dict, bam_dict):
+  """Compares a FASTA and BAM sequence dictionary, and prints any differences"""
+  common = set(fasta_dict) & set(bam_dict)
+  if not common:
+    sys.stderr.write("  No sequences in common!\n")
+    return
 
-  return True
+  different = []
+  for key in sorted(common):
+    if fasta_dict[key] != bam_dict[key]:
+      different.append((key, fasta_dict[key], bam_dict[key]))
 
+  if different:
+    sys.stderr.write("  Sequence length differs:\n")
+    for values in different:
+      sys.stderr.write("    - %s: %ibp vs %ibp\n" % values)
+
+  for (dd, name) in ((fasta_dict, "FASTA"), (bam_dict, "BAM")):
+    if set(dd) - common:
+      sys.stderr.write("  Only in %s dictionary:\n" % (name,))
+      for key in set(dd) - common:
+        sys.stderr.write("    - %s = %ibp\n" % (key, dd[key]))
