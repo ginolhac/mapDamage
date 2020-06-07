b718da4edbe85f53b2f33fd1e5535d679b13fc12
Mikkel Schubert <MikkelSch@gmail.com>
2020-06-07 16:12:34 +0200

    fix use of wrong filename in read_base_comp

--- mapdamage/composition.py
+++ mapdamage/composition.py
@@ -28,7 +28,7 @@ def write_base_comp(fasta, destination):
 def read_base_comp(filename):
     """Read the base compition from a file created by write_base_comp
     """
-    with open("names.csv", newline="") as csvfile:
+    with open(filename, newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             return row
